73e0f51196b55bf54781411805814513cf85f811
Mukund Gupta <mukundofficial07@gmail.com>
2021-07-08 13:12:12 +0530

    demo1

---
+++ build/precache-manifest.dd709ddd8749a9ad2d9db59554aa1cc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd668e9a8ab65d646b1e",
+    "url": "/Demo/static/css/main.72cdbf36.chunk.css"
+  },
+  {
+    "revision": "cd668e9a8ab65d646b1e",
+    "url": "/Demo/static/js/main.cd668e9a.chunk.js"
+  },
+  {
+    "revision": "c9b1be08ee4f3cc05d96",
+    "url": "/Demo/static/js/1.c9b1be08.chunk.js"
+  },
+  {
+    "revision": "07e11788f329c29232bf",
+    "url": "/Demo/static/js/runtime~main.07e11788.js"
+  },
+  {
+    "revision": "5f40c4e1da042159c9edaaf2ffae4669",
+    "url": "/Demo/index.html"
+  }
+];
